4f74933e431ba338f8a938f043ad813c6a6d5c9f
Manibharathi Subramani <Manibharathi.Subramani@expeditors.com>
2021-05-30 17:00:52 +0530

    Thread examples tested
    Thread examples tested

---
+++ src/com/datastructure/StackAndQueue/ImplementStackUsingQueue.java
@@ -0,0 +1,29 @@
+package com.datastructure.StackAndQueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ImplementStackUsingQueue {
+
+    /*
+    Methods of Stack -> pop(), peek(), add() -> LIFO
+    Methods of Queue -> dequeue(), enqueue() -> FIFO
+     */
+
+    public static Queue<Integer> queue1 = new LinkedList<>();
+    public static Queue<Integer> queue2 = new LinkedList<>();
+
+    /**
+     * Making the add operation costly
+     * @param x -> {8, 9, 5, 4, 6}
+     */
+
+    public void push(int x){
+
+
+
+
+    }
+
+
+}
